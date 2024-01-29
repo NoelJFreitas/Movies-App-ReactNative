22841f9f84a7d2a7467fa28e289562f30304859c
noel jose <noel@adderesolutions.com>
2024-01-29 17:05:07 -0300

    feat: config module aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@screens': './src/screens',
+          '@components': './src/components',
+          '@utils': './src/utils',
+          '@hooks': './src/hooks',
+          '@features': './src/features',
+          '@src': './src',
+        },
+      },
+    ],
+  ],
 };
