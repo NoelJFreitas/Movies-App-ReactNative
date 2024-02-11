c40b0033002481c8d388287a92b8a176ccb0bbda
noel jose <noel@adderesolutions.com>
2024-02-10 21:21:02 -0300

    feat: create signUpInput component

---
+++ src/features/authentication/components/TextInput/SignUpTextInput.tsx
@@ -0,0 +1,69 @@
+import commonStyles from '@features/authentication/utils/commonStyles';
+import colors from '@utils/colors';
+import React, {forwardRef, useImperativeHandle, useState} from 'react';
+import {StyleSheet, Text, TextInput, TextInputProps, View} from 'react-native';
+
+interface Props extends TextInputProps {
+  name: string;
+}
+
+interface SignUpTextInput {
+  setValidationState: (isValid: boolean) => void;
+  value: string | undefined;
+}
+
+const SignUpTextInput = forwardRef<SignUpTextInput, Props>(
+  ({name, value, ...props}: Props, ref) => {
+    const [isValid, setIsValid] = useState<undefined | boolean>(undefined);
+
+    function setValidationState(state: boolean) {
+      setIsValid(state);
+    }
+
+    useImperativeHandle(ref, () => ({
+      setValidationState,
+      value,
+    }));
+
+    return (
+      <View
+        style={[
+          styles.container,
+          commonStyles.shadow,
+          !isValid && isValid !== undefined && styles.invalid,
+        ]}>
+        {value && <Text style={styles.title}>{name}</Text>}
+        <TextInput
+          value={value}
+          placeholderTextColor={colors.grey}
+          style={commonStyles.text}
+          {...props}
+        />
+      </View>
+    );
+  },
+);
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 60,
+    backgroundColor: colors.white,
+    borderRadius: 7,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    position: 'absolute',
+    fontWeight: '400',
+    fontSize: 12,
+    left: 20,
+    top: -6,
+  },
+  invalid: {
+    borderColor: 'red',
+    borderWidth: 0.5,
+  },
+});
+
+export default SignUpTextInput;
