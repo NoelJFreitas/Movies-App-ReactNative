bfb6423cab3501fcac87f694b91eca936be741c3
noel jose <noel@adderesolutions.com>
2024-02-10 21:20:31 -0300

    feat: create text components

---
+++ src/components/Text/Title.tsx
@@ -0,0 +1,23 @@
+import colors from '@utils/colors';
+import fonts from '@utils/fonts';
+import React from 'react';
+import {Text as DefaultText, StyleSheet, TextProps} from 'react-native';
+
+interface Props extends TextProps {}
+
+export default function Title({children, style, ...props}: Props) {
+  return (
+    <DefaultText style={[styles.text, style]} {...props}>
+      {children}
+    </DefaultText>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: fonts.PoppinsRegular,
+    color: colors.black,
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
